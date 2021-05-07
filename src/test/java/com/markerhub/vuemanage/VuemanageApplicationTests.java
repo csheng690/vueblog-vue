1bec18891c1c3b1f378c51ac434520ddd9fd20d3
csheng <csheng690@163.com>
2021-05-08 02:11:00 +0800

    初始

---
+++ src/test/java/com/markerhub/vuemanage/VuemanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markerhub.vuemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuemanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
